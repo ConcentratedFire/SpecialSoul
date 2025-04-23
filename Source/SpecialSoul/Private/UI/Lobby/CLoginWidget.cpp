47712212eddd4b1e11e4493c755e3b23a08f7097
LHcom <55868914+LHcom@users.noreply.github.com>
2025-04-23 23:24:29 +0900

    로그인 위젯 작업중

---
+++ Source/SpecialSoul/Private/UI/Lobby/CLoginWidget.cpp
@@ -0,0 +1,59 @@
+#include "UI/Lobby/CLoginWidget.h"
+
+#include "Components/Button.h"
+#include "Components/EditableText.h"
+#include "Components/Image.h"
+
+void UCLoginWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	Txt_ID->OnTextChanged.AddDynamic(this, &UCLoginWidget::OnInputID);
+	Btn_LogIn->OnClicked.AddDynamic(this, &UCLoginWidget::OnLogin);
+
+	OrgBtnStyle.Normal.TintColor = OrgBgColor;
+	OrgBtnStyle.Normal.DrawAs = ESlateBrushDrawType::RoundedBox;
+	OrgBtnStyle.Normal.OutlineSettings.RoundingType = ESlateBrushRoundingType::FixedRadius;
+	OrgBtnStyle.Normal.OutlineSettings.CornerRadii = FVector4(10, 10, 10, 10);
+	OrgBtnStyle.Hovered.TintColor = OrgBgColor;
+	OrgBtnStyle.Hovered.DrawAs = ESlateBrushDrawType::RoundedBox;
+	OrgBtnStyle.Hovered.OutlineSettings.RoundingType = ESlateBrushRoundingType::FixedRadius;
+	OrgBtnStyle.Hovered.OutlineSettings.CornerRadii =  FVector4(10, 10, 10, 10);
+	OrgBtnStyle.Pressed.TintColor = OrgBgColor;
+	OrgBtnStyle.Pressed.DrawAs = ESlateBrushDrawType::RoundedBox;
+	OrgBtnStyle.Pressed.OutlineSettings.RoundingType = ESlateBrushRoundingType::FixedRadius;
+	OrgBtnStyle.Pressed.OutlineSettings.CornerRadii =  FVector4(10, 10, 10, 10);
+
+	ChangeBtnStyle.Normal.TintColor = ChangeBgColor;
+	ChangeBtnStyle.Normal.DrawAs = ESlateBrushDrawType::RoundedBox;
+	ChangeBtnStyle.Normal.OutlineSettings.RoundingType = ESlateBrushRoundingType::FixedRadius;
+	ChangeBtnStyle.Normal.OutlineSettings.CornerRadii =  FVector4(10, 10, 10, 10);
+	ChangeBtnStyle.Hovered.TintColor = ChangeBgHoverColor;
+	ChangeBtnStyle.Hovered.DrawAs = ESlateBrushDrawType::RoundedBox;
+	ChangeBtnStyle.Hovered.OutlineSettings.RoundingType = ESlateBrushRoundingType::FixedRadius;
+	ChangeBtnStyle.Hovered.OutlineSettings.CornerRadii =  FVector4(10, 10, 10, 10);
+	ChangeBtnStyle.Pressed.TintColor = ChangeBgHoverColor;
+	ChangeBtnStyle.Pressed.DrawAs = ESlateBrushDrawType::RoundedBox;
+	ChangeBtnStyle.Pressed.OutlineSettings.RoundingType = ESlateBrushRoundingType::FixedRadius;
+	ChangeBtnStyle.Pressed.OutlineSettings.CornerRadii =  FVector4(10, 10, 10, 10);
+
+	Btn_LogIn->SetStyle(OrgBtnStyle);
+}
+
+void UCLoginWidget::OnInputID(const FText& Text)
+{
+	if (Text.IsEmpty())
+	{
+		Btn_LogIn->SetStyle(OrgBtnStyle);
+		Img_Login->SetColorAndOpacity(OrgImgColor);
+	}
+	else
+	{
+		Btn_LogIn->SetStyle(ChangeBtnStyle);
+		Img_Login->SetColorAndOpacity(ChangeImgColor);
+	}
+}
+
+void UCLoginWidget::OnLogin()
+{
+	if (Txt_ID->GetText().IsEmpty()) return;
+}
---
+++ Source/SpecialSoul/Public/UI/Lobby/CLoginWidget.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CLoginWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPECIALSOUL_API UCLoginWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+	virtual void NativeConstruct() override;
+
+public:
+	UFUNCTION()
+	void OnLogin();
+	UFUNCTION()
+	void OnInputID(const FText& Text);
+
+private:
+	UPROPERTY(EditDefaultsOnly, meta=(BindWidget))
+	class UEditableText* Txt_ID;
+	UPROPERTY(EditDefaultsOnly, meta=(BindWidget))
+	class UButton* Btn_LogIn;
+	UPROPERTY(EditDefaultsOnly, meta=(BindWidget))
+	class UImage* Img_Login;
+
+	FLinearColor OrgBgColor = FLinearColor(.5, .5, .5, 1);
+	FLinearColor OrgImgColor = FLinearColor(.5, .5, .5, 1);
+	FLinearColor ChangeBgColor = FLinearColor(1, 0, 0, 1);
+	FLinearColor ChangeBgHoverColor = FLinearColor(.7, 0, 0, 1);
+	FLinearColor ChangeImgColor = FLinearColor(1, 1, 1, 1);
+
+	FButtonStyle OrgBtnStyle;
+	FButtonStyle ChangeBtnStyle;
+};

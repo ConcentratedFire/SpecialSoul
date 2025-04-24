7f3363e77cbb00e8ae3be828fdbf8ed97937df13
LHcom <55868914+LHcom@users.noreply.github.com>
2025-04-24 21:05:42 +0900

    헤더 위치 오류 수정

--- Source/SpecialSoul/Private/UI/Lobby/CSessionSlotWidget.cpp
+++ Source/SpecialSoul/Private/UI/Lobby/CSessionSlotWidget.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Game/CGameInstance.h"
 #include "UI/Lobby/CSessionSlotWidget.h"
+#include "Game/CGameInstance.h"
 
 #include "Components/Border.h"
 #include "Components/TextBlock.h"
